71d577c63e2bdb7c0965c157e93760badfd8ee91
hasan-furkan <hsnfrkn32@gmail.com>
2022-11-04 17:36:28 +0300

    commit

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import {Link, Outlet} from "react-router-dom";
+import {Fragment} from "react";
+
+import "./navigation.styles.scss"
+
+import {ReactComponent as Crwnlogo} from "../../assets/crown.svg";
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className="navigation">
+               <Link to="/" className="logo-container">
+                   <Crwnlogo />
+               </Link>
+                <div className="nav-links-container">
+                    <Link to="/shop" className="nav-link">
+                        SHOP
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default Navigation
