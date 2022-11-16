aae4e694b01cebfd1c71dce40401485694e7767f
hasan-furkan <hsnfrkn32@gmail.com>
2022-11-16 15:25:55 +0300

    adding cart-icon, cart-dropdown and cart.context

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 import App from "./App";
-import { UserProvider } from "./contexts/user.context";
+import { CartProvider } from "./contexts/cart.context";
 import { ProductsProvider } from "./contexts/products.context";
+import { UserProvider } from "./contexts/user.context";
 
 import reportWebVitals from "./reportWebVitals";
 
@@ -16,7 +17,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
